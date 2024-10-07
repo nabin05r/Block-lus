858803bdcc390d556d3f585c17effc03a5c6dd99
nabin05r <nabin.magar05r@gmail.com>
2024-10-07 18:15:09 +0545

    Plugin to add blocks for Gutenberg

---
+++ block-plus.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Plugin Name:       Block Plus
+ * Plugin URI:        https://nabinmagar.com
+ * Description:       Provides more blocks to the Gutenberg Editor.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            Nabin Gharti Magar
+ * Author URI:        https://nabinmagar.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       block-plus
+ * Domain Path:       /languages
+ */
+
+if(! defined('ABSPATH')){
+    die;
+}
+
+if( ! class_exists('Block_Plus')){
+
+    class Block_Plus{
+
+
+    }
+
+}
+
+
